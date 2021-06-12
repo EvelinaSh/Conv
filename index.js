ee6a8470b6dcf3fef0fcea547ffa45874785bdd4
EvelinaSh <mikstura98@mail.ru>
2021-06-12 12:39:04 +0500

    update index

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, '../view/public')))
 app.use('/api', router)
 
 app.use(errorHandler)
