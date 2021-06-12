21cf2c1c009f952295b786d4676d6a2502ce998f
EvelinaSh <mikstura98@mail.ru>
2021-06-12 11:56:09 +0500

    update db.js

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-        ssl: true
+        ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
     }
 });
