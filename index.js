75ec7b3f66369b496bcd2eff4cc4ddc7e7a718ea
EvelinaSh <mikstura98@mail.ru>
2021-06-12 11:31:11 +0500

    ver_1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        app.listen(process.env.PORT)
     } catch (e) {
         console.log(e)
     }
